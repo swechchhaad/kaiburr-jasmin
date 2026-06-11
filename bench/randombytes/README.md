096b4bb5610c904bb115aba18777f231a9b591d8
swechchhaad <swechchha.adhikari4@gmail.com>
2026-06-11 17:51:44 +0000

    Initial commit

--- bench/randombytes/README.md
+++
@@ -1,1 +0,0 @@
-TODO: write this README file explaining the contents of this folder and how it should be used and not used;
--- bench/randombytes/randombytes1.h
+++
@@ -1,8 +0,0 @@
-#ifndef RANDOMBYTES_H
-#define RANDOMBYTES_H
-
-#include <stdint.h>
-
-int randombytes(uint8_t* x, uint64_t xlen);
-
-#endif
